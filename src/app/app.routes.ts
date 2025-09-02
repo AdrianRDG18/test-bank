e4a34bf5fb6ab06df245e9927ba8c3f1982d29ff
AdrianRDG <adrian.rojas@tergar.org>
2025-09-02 08:24:40 -0600

    Feat: add inital template for withdrawal paga & route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/withdrawal-page/withdrawal-page').then(m => m.WithdrawalPage)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/pages/withdrawal-page/withdrawal-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-withdrawal-page',
+  imports: [],
+  templateUrl: './withdrawal-page.html',
+  styles: ``
+})
+export class WithdrawalPage {
+
+}
